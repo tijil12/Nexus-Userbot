38ad9cc54641e5a3e996c6bdf7b9b45d1d50b104
tijil12 <shadowxempire0@gmail.com>
2026-02-20 19:17:41 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Installing FFmpeg..."
-curl -L https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz | tar -xJ
-export PATH=$PATH:$(pwd)/ffmpeg-master-latest-linux64-gpl/bin
-
-echo "Starting Bot..."
-python main.py
